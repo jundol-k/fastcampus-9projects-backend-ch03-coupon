a79da0d1a3543934bb3a7d855a11caa9541dc623
jundol <jundol8791@gmail.com>
2024-02-18 21:17:25 +0900

    redis script를 이용한 성능 확보

--- coupon-api/src/main/java/com/example/couponapi/service/CouponIssueRequestService.java
+++ coupon-api/src/main/java/com/example/couponapi/service/CouponIssueRequestService.java
@@ -3,6 +3,7 @@ package com.example.couponapi.service;
 import com.example.couponapi.controller.dto.CouponIssueRequestDto;
 import com.example.couponcore.component.DistributeLockExecutor;
 import com.example.couponcore.service.AsyncCouponIssueServiceV1;
+import com.example.couponcore.service.AsyncCouponIssueServiceV2;
 import com.example.couponcore.service.CouponIssueService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -16,6 +17,7 @@ public class CouponIssueRequestService {
     private final CouponIssueService couponIssueService;
     private final DistributeLockExecutor distributeLockExecutor;
     private final AsyncCouponIssueServiceV1 asyncCouponIssueServiceV1;
+    private final AsyncCouponIssueServiceV2 asyncCouponIssueServiceV2;
 
     public void issueRequestV1(CouponIssueRequestDto requestDto) {
         //distributeLockExecutor.execute("lock_" + requestDto.couponId(), 1000, 10000, () -> {
@@ -26,6 +28,11 @@ public class CouponIssueRequestService {
 
     public void asyncIssueRequestV1(CouponIssueRequestDto requestDto) {
         asyncCouponIssueServiceV1.issue(requestDto.couponId(), requestDto.userId());
+        log.info("쿠폰 발급 완료.  couponId: %s, userId: %s".formatted(requestDto.couponId(), requestDto.userId()));
+    }
+
+    public void asyncIssueRequestV2(CouponIssueRequestDto requestDto) {
+        asyncCouponIssueServiceV2.issue(requestDto.couponId(), requestDto.userId());
         log.info("쿠폰 발급 완료.  couponId: %s, userId: %s".formatted(requestDto.couponId(), requestDto.userId()));
     }
 }
---
+++ coupon-core/src/main/java/com/example/couponcore/service/AsyncCouponIssueServiceV2.java
@@ -0,0 +1,28 @@
+package com.example.couponcore.service;
+
+import com.example.couponcore.repository.redis.RedisRepository;
+import com.example.couponcore.repository.redis.dto.CouponRedisEntity;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+    public class AsyncCouponIssueServiceV2 {
+
+    private final RedisRepository redisRepository;
+    private final CouponCacheService couponCacheService;
+
+    // set을 이용한 방법
+    public void issue(long couponId, long userId) {
+        CouponRedisEntity coupon = couponCacheService.getCouponCache(couponId);
+        coupon.checkIssuableCoupon();
+        issueRequest(couponId, userId, coupon.totalQuantity());
+    }
+
+    private void issueRequest(long couponId, long userId, Integer totalIssueQuantity) {
+        if (totalIssueQuantity == null) {
+            redisRepository.issueRequest(couponId, userId, Integer.MAX_VALUE);
+        }
+        redisRepository.issueRequest(couponId, userId, totalIssueQuantity);
+    }
+}
