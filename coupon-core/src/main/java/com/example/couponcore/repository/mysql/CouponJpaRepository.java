36b595a4903f69db3912eab7c4ebba8d71937dfa
jundol <jundol8791@gmail.com>
2024-02-18 21:47:20 +0900

    쿠폰 발급 서버 구현

--- coupon-core/src/main/java/com/example/couponcore/repository/mysql/CouponJpaRepository.java
+++ coupon-core/src/main/java/com/example/couponcore/repository/mysql/CouponJpaRepository.java
@@ -5,6 +5,7 @@ import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Lock;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
@@ -12,5 +13,5 @@ public interface CouponJpaRepository extends JpaRepository<Coupon, Long> {
 
     @Lock(LockModeType.PESSIMISTIC_WRITE)
     @Query("SELECT c FROM Coupon c WHERE c.id = :id")
-    Optional<Coupon> findCouponWithLock(long id);
+    Optional<Coupon> findCouponWithLock(@Param("id") long id);
 }
